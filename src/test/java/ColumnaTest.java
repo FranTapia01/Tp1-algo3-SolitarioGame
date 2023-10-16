aecae22eec6c6daf5476fa1d7b5208fa1f19a465
Barroero <IBarroero@fi.uba.ar>
2023-10-16 15:19:03 -0300

    Tests de columna actualizados a los cambios en columna

--- src/test/java/ColumnaTest.java
+++ src/test/java/ColumnaTest.java
@@ -6,61 +6,115 @@ import static org.junit.Assert.*;
 
 public class ColumnaTest {
 
-    @Test
+    @Test // Agregar carta válida a columna vacia
     public void Test1() {
-        var carta1 = new Carta(9, Carta.Palo.DIAMANTE);
-        var carta2 = new Carta(13, Carta.Palo.PICA);
-        var carta3 = new Carta(1, Carta.Palo.TREBOL);
-
+        var carta1 = new Carta(13, Carta.Palo.DIAMANTE);
         var cartas = new ArrayList<Carta>();
         var pile = new Pile();
-        pile.add(carta1);
-        pile.add(carta2);
-        pile.add(carta3);
-        cartas.add(carta1);
-        cartas.add(carta2);
-
         Columna col = new Columna(pile);
+        cartas.add(carta1);
+
         col.agregarCartas(cartas);
 
-        assertEquals(col.getCartasVisibles().size(), 3);
-        assertEquals(col.getCartasVisibles().get(0), carta3);
+        assertEquals(col.getCartasVisibles().size(), 1);
+        assertEquals(col.getCartasVisibles().get(0), carta1);
 
     }
 
-    @Test
+    @Test // Agregar carta inválida a columna vacia
     public void Test2() {
-        var carta1 = new Carta(9, Carta.Palo.DIAMANTE);
-        var carta2 = new Carta(13, Carta.Palo.PICA);
-        var carta3 = new Carta(3, Carta.Palo.TREBOL);
+        var carta1 = new Carta(12, Carta.Palo.DIAMANTE);
+        var cartas = new ArrayList<Carta>();
+        var pile = new Pile();
+        Columna col = new Columna(pile);
+        cartas.add(carta1);
 
+        col.agregarCartas(cartas);
+
+        assertEquals(col.getCartasVisibles().size(), 0);
+
+    }
+    @Test // Agregar carta válida
+    public void Test3() {
+        var carta1 = new Carta(11, Carta.Palo.TREBOL);
+        var carta2 = new Carta(7, Carta.Palo.CORAZON);
+        var carta3 = new Carta(6, Carta.Palo.PICA);
         var cartas = new ArrayList<Carta>();
         var pile = new Pile();
         pile.push(carta1);
         pile.push(carta2);
         cartas.add(carta3);
-
         Columna col = new Columna(pile);
-        col.agregarCartas(cartas);
-        var sobra = new ArrayList<>(col.sacarCartas(2));
 
-        assertEquals(col.getCartasVisibles().size(), 1);
-        assertEquals(col.getCartasVisibles().get(0), carta1);
-        assertEquals(sobra.get(0), carta2);
+        col.agregarCartas(cartas);
+
+        assertEquals(col.getCartasVisibles().size(), 2);
+        assertEquals(col.getCartasVisibles().get(1), carta3);
     }
 
-    @Test
-    public void Test3() {
-        var carta1 = new Carta(9, Carta.Palo.DIAMANTE);
-        var carta2 = new Carta(13, Carta.Palo.PICA);
-
+    @Test // Agregar carta inválida
+    public void Test4() {
+        var carta1 = new Carta(13, Carta.Palo.PICA);
+        var carta2 = new Carta(11, Carta.Palo.TREBOL);
+        var carta3 = new Carta(7, Carta.Palo.CORAZON);
+        var cartas = new ArrayList<Carta>();
         var pile = new Pile();
         pile.push(carta1);
         pile.push(carta2);
-
+        cartas.add(carta3);
         Columna col = new Columna(pile);
 
-        assertEquals(col.getCantidadCartasNoVisibles(), 1);
+        col.agregarCartas(cartas);
 
+        assertEquals(col.getCartasVisibles().size(), 1);
     }
+
+    @Test // Agregar cartas válidas
+    public void Test5() {
+        var carta1 = new Carta(11, Carta.Palo.TREBOL);
+        var carta2 = new Carta(7, Carta.Palo.CORAZON);
+        var carta3 = new Carta(6, Carta.Palo.PICA);
+        var carta4 = new Carta(9, Carta.Palo.TREBOL);
+        var cartas = new ArrayList<Carta>();
+        var pile = new Pile();
+        pile.push(carta1);
+        pile.push(carta2);
+        cartas.add(carta3);
+        cartas.add(carta4);
+        Columna col = new Columna(pile);
+
+        col.agregarCartas(cartas);
+
+        assertEquals(col.getCartasVisibles().size(), 3);
+        assertEquals(col.getCartasVisibles().get(2), carta4);
+    }
+
+    @Test // Sacar cartas de columna con cartas
+    public void Test6() {
+        var carta1 = new Carta(3, Carta.Palo.CORAZON);
+        var carta2 = new Carta(7, Carta.Palo.TREBOL);
+        var carta3 = new Carta(13, Carta.Palo.CORAZON);
+        var carta4 = new Carta(12, Carta.Palo.PICA);
+        var carta5 = new Carta(8, Carta.Palo.DIAMANTE);
+        var cartas = new ArrayList<Carta>();
+        var pile = new Pile();
+        pile.push(carta1);
+        pile.push(carta2);
+        pile.push(carta3);
+        cartas.add(carta4);
+        cartas.add(carta5);
+        Columna col = new Columna(pile);
+        col.agregarCartas(cartas);
+
+        var cartasSacadas = col.sacarCartas(3);
+
+        assertEquals(col.getCartasVisibles().size(), 1);
+        assertEquals(col.getCantidadCartasNoVisibles(), 1);
+        assertEquals(cartasSacadas.size(), 3);
+        assertEquals(cartasSacadas.get(0), carta3);
+        assertEquals(cartasSacadas.get(1), carta4);
+        assertEquals(cartasSacadas.get(2), carta5);
+    }
+
+
 }
